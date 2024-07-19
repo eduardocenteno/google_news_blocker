6f9500a10156d39d652bf79d7afc41e61303f873
eduardocenteno <107418860+eduardocenteno@users.noreply.github.com>
2024-07-18 22:21:40 -0600

    Update content.js

--- source/content.js
+++ source/content.js
@@ -1,15 +1,8 @@
 const toggleGoogleNews = () => {
   chrome.storage.sync.get(['hideNews'], function (data) {
-    if (data.hideNews) {
-      const newsTab = document.querySelector('a.XIzzdf div.YmvwI[aria-current="page"]');
-      if (newsTab && (newsTab.textContent === "News" || newsTab.textContent === "Noticias")) {
-        newsTab.parentElement.style.display = 'none';
-      }
-    } else {
-      const newsTab = document.querySelector('a.XIzzdf div.YmvwI[aria-current="page"]');
-      if (newsTab && (newsTab.textContent === "News" || newsTab.textContent === "Noticias")) {
-        newsTab.parentElement.style.display = '';
-      }
+    const newsTab = document.querySelector('a.XIzzdf div.YmvwI[aria-current="page"]');
+    if (newsTab && (newsTab.textContent === "News" || newsTab.textContent === "Noticias")) {
+      newsTab.parentElement.style.display = data.hideNews ? 'none' : '';
     }
   });
 };
