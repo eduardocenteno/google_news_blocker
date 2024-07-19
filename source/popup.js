cb58c18270ea18a08a996d93ffa6e6bc6661fd49
eduardocenteno <107418860+eduardocenteno@users.noreply.github.com>
2024-07-18 22:06:06 -0600

    Update popup.js

--- source/popup.js
+++ source/popup.js
@@ -1,18 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const toggleFeed = document.getElementById('toggleFeed');
   const toggleNews = document.getElementById('toggleNews');
 
   // Load the current state from storage
-  chrome.storage.sync.get(['hideFeed', 'hideNews'], function (data) {
-    toggleFeed.checked = data.hideFeed || false;
+  chrome.storage.sync.get(['hideNews'], function (data) {
     toggleNews.checked = data.hideNews || false;
   });
 
   // Save the state to storage when the checkbox is toggled
-  toggleFeed.addEventListener('change', function () {
-    chrome.storage.sync.set({ hideFeed: toggleFeed.checked });
-  });
-
   toggleNews.addEventListener('change', function () {
     chrome.storage.sync.set({ hideNews: toggleNews.checked });
   });
