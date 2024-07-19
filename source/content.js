39e476d88dd76e9bc41a960036d32cc293723e97
eduardocenteno <107418860+eduardocenteno@users.noreply.github.com>
2024-07-18 22:01:45 -0600

    Update content.js

--- source/content.js
+++ source/content.js
@@ -1,9 +1,16 @@
-const removeNewsTab = () => {
-  const newsTab = document.querySelector('a.XIzzdf div.YmvwI[aria-current="page"]');
-  if (newsTab && (newsTab.textContent === "News" || newsTab.textContent === "Noticias")) {
-    newsTab.parentElement.style.display = 'none';
-  }
+const removeTabs = () => {
+  chrome.storage.sync.get(['hideFeed', 'hideNews'], function (data) {
+    if (data.hideFeed) {
+      // Hide feed logic here
+    }
+    if (data.hideNews) {
+      const newsTab = document.querySelector('a.XIzzdf div.YmvwI[aria-current="page"]');
+      if (newsTab && (newsTab.textContent === "News" || newsTab.textContent === "Noticias")) {
+        newsTab.parentElement.style.display = 'none';
+      }
+    }
+  });
 };
 
 // Run the function when the DOM content is loaded
-document.addEventListener('DOMContentLoaded', removeNewsTab);
+document.addEventListener('DOMContentLoaded', removeTabs);
